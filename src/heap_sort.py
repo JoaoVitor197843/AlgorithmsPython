6496f6aacde20b32fe6e45aaba9711ac47604214
GhostPro1736 <joaovirorconceicaodesoza21@gmail.com>
2025-04-19 17:09:56 -0300

    Now heap sort class change the real list, not only a copy, altered variable name's, altered heapify function to heapify_down

--- src/heap_sort.py
+++ src/heap_sort.py
@@ -7,36 +7,31 @@ class heap_sort(binary_heap):
     max_heapify: view the function in make_binary_heap
     heapify_limit: the last parent node
     heapify_steps: the ready part of all list"""
-    self.items = to_sort
-    self.end_index = len(self.items) - 1
-    self._max_heapify()
-    print(self.items)
-    self.last_parent_limit = self._get_last_parent_node()
+    self.end_index = len(to_sort) - 1
+    self._max_heapify(to_sort)
+    self.last_parent_limit = self._get_last_parent_node(to_sort)
     while self.end_index > 0:
-      if self.items[0] > self.items[self.end_index]:
-        self.items[0], self.items[self.end_index] = self.items[self.end_index], self.items[0]
-        self._heapify_after_sort()
+      if to_sort[0] > to_sort[self.end_index]:
+        to_sort[0], to_sort[self.end_index] = to_sort[self.end_index], to_sort[0]
+        self._heapify_after_sort(to_sort)
       self.end_index -= 1
-  def __str__(self):
-    """This only return the list, str can only return a string"""
-    return str(self.items)
-  def _heapify_after_sort(self):
+  def _heapify_after_sort(self, to_sort):
     """This make a new heapify after a part of the sort
     largest: the greater number
     current item: the current node index
     left, right: left and right childs"""
-    largest = 0
     current_item = 0
-    while largest <= self.end_index - 1:
+    while current_item <= self.end_index - 1:
+      largest = current_item
       left, right = self._get_left_child_node(largest), self._get_right_child_node(largest)
       if left >= self.end_index - 1:
         break
-      if self.items[largest] < self.items[left]:
+      if to_sort[largest] < to_sort[left]:
         largest = left      
-      if right != -1 and self.items[largest] < self.items[right]:
+      if right <= len(to_sort) and to_sort[largest] < to_sort[right]:
         largest = right
-      if self.items[largest] != self.items[current_item]:
-        self.items[current_item], self.items[largest] = self.items[largest], self.items[current_item]
-        current_item = largest
-      else:
+      if largest == current_item:
         break
+      else:
+        to_sort[current_item], to_sort[largest] = to_sort[largest], to_sort[current_item]
+        current_item = largest
--- src/make_binary_heap.py
+++ src/make_binary_heap.py
@@ -1,65 +1,61 @@
 class binary_heap:
-    def _get_last_parent_node(self) -> int:
+    @staticmethod
+    def _get_last_parent_node(to_sort) -> int:
       """this function get's the last parent node"""
-      return (len(self.items) // 2) - 1
-    def _get_left_child_node(self, parent_node: int) -> int:
+      return (len(to_sort) // 2) - 1
+    @staticmethod
+    def _get_left_child_node(parent_node: int) -> int:
        """This function get the left child node
        
        Parameters:
        
        parent node(int): the index of the parent node"""
        return (parent_node * 2) + 1
-    def _get_right_child_node(self, parent_node: int) -> int:
+    @staticmethod
+    def _get_right_child_node(parent_node: int, ) -> int:
       """This function get the right child node, return -1 if the parent don't have a right child
       
       Parameters:
       
       parent node(int): the index of the parent node"""
-      return (parent_node * 2) + 2 if (parent_node * 2) + 2 < len(self.items) else -1
-    def _heapify(self, current_node: int) -> None | int:
+      return (parent_node * 2) + 2
+    def _heapify_down(self, current_node: int, to_sort) -> None | int:
       """This function makes the changed node go to the right place after replaced
 
       Parameters:
 
       current node(int): the index of the changed node to heapify again"""
-      if current_node > self._get_last_parent_node():
-        return None
-      candidate_index = current_node
-      left_child, right_child = self._get_left_child_node(candidate_index), self._get_right_child_node(candidate_index)
-      if self.items[left_child] > self.items[candidate_index]:
-        current_node = left_child
-      if right_child != -1:
-        if self.items[right_child] > self.items[current_node]:
-          current_node = right_child
-      if current_node != candidate_index:
-        self.items[candidate_index], self.items[current_node] = self.items[current_node], self.items[candidate_index]
-        return current_node
+      largest_index = current_node
+      left_child, right_child = self._get_left_child_node(largest_index), self._get_right_child_node(largest_index)
+      if left_child < len(to_sort) and to_sort[left_child] > to_sort[largest_index]:
+        largest_index = left_child
+      if right_child < len(to_sort) and to_sort[right_child] > to_sort[largest_index]:
+        largest_index = right_child
+      if current_node != largest_index:
+        to_sort[largest_index], to_sort[current_node] = to_sort[current_node], to_sort[largest_index]
+        return largest_index
       else:
         return None
-    def _max_heapify(self):
+    def _max_heapify(self, to_sort):
       """This function makes the binary heap, it's make a max heapify, but there is also a min heapify
       heap_last_parent: get the last node with childs
       current index: get the current node to verify
       left/right child: get the left and the right child(if the parent have a right child)
       heapified index: makes the changed child get in the right place after replaced"""
-      heap_last_parent = self._get_last_parent_node()
-      while heap_last_parent >= 0:
-        largest_index = heap_last_parent
-        left_child, right_child = self._get_left_child_node(heap_last_parent), self._get_right_child_node(heap_last_parent)
-        if self.items[largest_index] < self.items[left_child]:
-            largest_index = left_child
-        if right_child != -1:
-          if self.items[largest_index] < self.items[right_child]:
-            largest_index = right_child
-        if self.items[largest_index] != self.items[heap_last_parent]:
-          self.items[heap_last_parent], self.items[largest_index] = self.items[largest_index], self.items[heap_last_parent]
-          while True:
-            heapified_index = self._heapify(largest_index)
-            if heapified_index is None:
-              break
-            else:
-              largest_index = heapified_index
-        heap_last_parent -= 1
+      current_node = self._get_last_parent_node(to_sort)
+      while current_node >= 0:
+        largest_index = current_node
+        left_child, right_child = self._get_left_child_node(current_node), self._get_right_child_node(current_node)
+        if to_sort[largest_index] < to_sort[left_child]:
+          largest_index = left_child
+        if right_child < len(to_sort) and to_sort[largest_index] < to_sort[right_child]:
+          largest_index = right_child
+        if largest_index != current_node:
+          to_sort[current_node], to_sort[largest_index] = to_sort[largest_index], to_sort[current_node]
+          adjust_node = largest_index
+          while adjust_node != None:
+            adjust_node = self._heapify_down(adjust_node, to_sort)
+        current_node -= 1
 class list_verification:
   @staticmethod
   def __init__(to_verify_list: list[int]) -> bool | ValueError | IndexError | TypeError:
